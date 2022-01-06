0c5f18ae2842b90e81e59d4d27f16ed30d5d861e
autofelix <autofelix@163.com>
2022-01-07 00:34:43 +0800

    图片转换为字符图片

---
+++ charset_picture/main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+#-------------------------------------------------------------------
+#                   CONFIDENTIAL --- CUSTOM STUDIOS
+#-------------------------------------------------------------------
+#
+#                   @Project Name : 图片转换为字符图片
+#
+#                   @File Name    : main.py
+#
+#                   @Programmer   : autofelix
+#
+#                   @Start Date   : 2021-01-07 13:14
+#
+#                   @Last Update  : 2021-01-07 13:14
+#
+#-------------------------------------------------------------------
+'''
+from PIL import Image
+
+class wordCloud:
+    '''
+     This is a main Class, the file contains all documents.
+     One document contains paragraphs that have several sentences
+     It loads the original file and converts the original file to new content
+     Then the new content will be saved by this class
+    '''
+    def __init__(self):
+        self.char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+        self.width = 60
+        self.height = 45
+
+    def hello(self):
+        '''
+        This is a welcome speech
+        :return: self
+        '''
+        print('*' * 50)
+        print(' ' * 18 + '图片转换为字符图片')
+        print(' ' * 5 + 'Author: autofelix  Date: 2021-01-07 13:14')
+        print('*' * 50)
+        return self
+
+    def get_char(self, r, g, b, alpha=256):
+        '''
+        将256灰度映射到70个字符上，也就是RGB值转字符的函数
+        :alpha: 透明度
+        :return: self
+        '''
+        if alpha == 0:
+            return ' '
+        length = len(self.char)
+        gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+        unit = (256.0 + 1) / length
+        return self.char[int(gray / unit)]
+
+    def run(self):
+        '''
+        The program entry
+        '''
+        im = Image.open('assets/back.jpeg')
+        im = im.resize((self.width, self.height), Image.NEAREST)
+        txt = ""
+        for i in range(self.height):
+            for j in range(self.width):
+                txt += self.get_char(*im.getpixel((j, i)))
+            txt += '\n'
+        print(txt)
+        with open('handler.txt', 'w') as f:
+            f.write(txt)
+
+if __name__ == '__main__':
+    wordCloud().hello().run()
+
+
---
+++ digital_picture/main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+#-------------------------------------------------------------------
+#                   CONFIDENTIAL --- CUSTOM STUDIOS
+#-------------------------------------------------------------------
+#
+#                   @Project Name : 数字画像
+#
+#                   @File Name    : main.py
+#
+#                   @Programmer   : autofelix
+#
+#                   @Start Date   : 2021-01-06 13:14
+#
+#                   @Last Update  : 2021-01-06 13:14
+#
+#-------------------------------------------------------------------
+'''
+from wordcloud import WordCloud
+import cv2, jieba
+import matplotlib.pyplot as plt
+
+class wordCloud:
+    '''
+     This is a main Class, the file contains all documents.
+     One document contains paragraphs that have several sentences
+     It loads the original file and converts the original file to new content
+     Then the new content will be saved by this class
+    '''
+    def __init__(self):
+        self.bg_img = 'assets/back.jpeg'
+        self.word_path = 'assets/word.txt'
+
+    def hello(self):
+        '''
+        This is a welcome speech
+        :return: self
+        '''
+        print('*' * 50)
+        print(' ' * 25 + '数字画像')
+        print(' ' * 5 + 'Author: autofelix  Date: 2021-01-06 13:14')
+        print('*' * 50)
+        return self
+
+    def run(self):
+        '''
+        The program entry
+        '''
+        with open(self.word_path, 'r') as f:
+            word = f.read()
+
+        cut_word = ' '.join(jieba.cut(word))
+        color_mask = cv2.imread(self.bg_img)
+
+        word_cloud = WordCloud(
+            # 设置字体，不指定就会出现乱码
+            font_path='/System/Library/Fonts/PingFang.ttc',
+            # 设置背景色
+            background_color='white',
+            # 词云形状
+            mask=color_mask,
+            # 允许最大词汇
+            max_words=120,
+            # 最大号字体
+            max_font_size=2000
+        )
+
+        result = word_cloud.generate(cut_word)
+        result.to_file('word_cloud.jpg')
+        plt.imshow(result, interpolation='bilinear')
+        plt.axis('off')
+        plt.show()
+
+
+if __name__ == '__main__':
+    wordCloud().hello().run()
+
+
