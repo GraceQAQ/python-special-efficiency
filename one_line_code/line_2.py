9d7c64bc37868b11d808cd75ee64e2e09e779a62
autofelix <autofelix@163.com>
2022-01-13 17:14:18 +0800

    一行代码之打印芭蕉扇

--- one_line_code/line_2.py
+++ one_line_code/line_2.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+#-------------------------------------------------------------------
+#                   CONFIDENTIAL --- CUSTOM STUDIOS
+#-------------------------------------------------------------------
+#
+#                   @Project Name : 一行代码之打印芭蕉扇
+#
+#                   @File Name    : line_2.py
+#
+#                   @Programmer   : autofelix
+#
+#                   @Start Date   : 2022/01/13 13:14
+#
+#                   @Last Update  : 2022/01/13 13:14
+#
+#-------------------------------------------------------------------
+'''
 print('\n'.join([''.join(['*'if abs((lambda a:lambda z,c,n:a(a,z,c,n))(lambda s,z,c,n:z if n==0else s(s,z*z+c,c,n-1))(0,0.02*x+0.05j*y,40))<2 else' 'for x in range(-80,20)])for y in range(-20,20)]))
