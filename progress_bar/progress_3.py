438fd715a466b6f0d29a417eeef22f1872a03b53
autofelix <autofelix@163.com>
2022-01-13 11:19:39 +0800

    可视化进度条三

--- progress_bar/progress_3.py
+++ progress_bar/progress_3.py
@@ -1,8 +1,55 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+#-------------------------------------------------------------------
+#                   CONFIDENTIAL --- CUSTOM STUDIOS
+#-------------------------------------------------------------------
+#
+#                   @Project Name : 可视化进度条三
+#
+#                   @File Name    : progress_3.py
+#
+#                   @Programmer   : autofelix
+#
+#                   @Start Date   : 2022/01/13 13:14
+#
+#                   @Last Update  : 2022/01/13 13:14
+#
+#-------------------------------------------------------------------
+'''
 from time import sleep
 from tqdm import tqdm
-# 这里同样的，tqdm就是这个进度条最常用的一个方法
-# 里面存一个可迭代对象
-for i in tqdm(range(1, 500)):
- # 模拟你的任务
- sleep(0.01)
-sleep(0.5)
+
+class progress:
+    '''
+     This is a main Class, the file contains all documents.
+     One document contains paragraphs that have several sentences
+     It loads the original file and converts the original file to new content
+     Then the new content will be saved by this class
+    '''
+    def __init__(self):
+        pass
+
+    def hello(self):
+        '''
+        This is a welcome speech
+        :return: self
+        '''
+        print('*' * 50)
+        print(' ' * 20 + '可视化进度条三')
+        print(' ' * 5 + '作者: autofelix  Date: 2022-01-13 13:14')
+        print(' ' * 5 + '主页: https://autofelix.blog.csdn.net')
+        print('*' * 50)
+        return self
+
+    def run(self):
+        '''
+        The program entry
+        '''
+        for i in tqdm(range(1, 500)):
+            # 模拟你的任务
+            sleep(0.01)
+        sleep(0.5)
+
+if __name__ == '__main__':
+    progress().hello().run()
