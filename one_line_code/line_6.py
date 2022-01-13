7c97dadd2f61d43e88a8b3fc4e7ca9bd32eb62af
autofelix <autofelix@163.com>
2022-01-13 18:30:12 +0800

    一行代码之打造迷宫

--- one_line_code/line_6.py
+++ one_line_code/line_6.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+#-------------------------------------------------------------------
+#                   CONFIDENTIAL --- CUSTOM STUDIOS
+#-------------------------------------------------------------------
+#
+#                   @Project Name : 一行代码之打造迷宫
+#
+#                   @File Name    : line_6.py
+#
+#                   @Programmer   : autofelix
+#
+#                   @Start Date   : 2022/01/13 13:14
+#
+#                   @Last Update  : 2022/01/13 13:14
+#
+#-------------------------------------------------------------------
+'''
 print(''.join(__import__('random').choice('/\\') for i in range(50*24)))
