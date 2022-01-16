9e83a3d70336c6edbb1134621530d3183ff315a5
autofelix <autofelix@163.com>
2022-01-16 18:39:45 +0800

    高级图片转换特效之流年

--- picture_effects_conversion_pro/effects_7.py
+++ picture_effects_conversion_pro/effects_7.py
@@ -17,7 +17,7 @@
 #
 #-------------------------------------------------------------------
 '''
-import cv2
+import cv2, math
 import numpy as np
 
 class picture:
@@ -43,7 +43,28 @@ class picture:
         return self
 
     def run(self):
-        pass
+        # 读取原始图像
+        img = cv2.imread(self.path)
+        # 获取图像行和列
+        rows, cols = img.shape[:2]
+        # 新建目标图像
+        dst = np.zeros((rows, cols, 3), dtype="uint8")
+        # 图像流年特效
+        for i in range(rows):
+            for j in range(cols):
+                # B通道的数值开平方乘以参数12
+                B = math.sqrt(img[i, j][0]) * 12
+                G = img[i, j][1]
+                R = img[i, j][2]
+                if B > 255:
+                    B = 255
+                dst[i, j] = np.uint8((B, G, R))
+
+        # 显示图像
+        cv2.imshow('src', img)
+        cv2.imshow('dst', dst)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     picture().hello().run()
